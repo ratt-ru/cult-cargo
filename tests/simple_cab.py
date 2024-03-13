a7df998867a004beef01b32e85ceabdf5ce8eb26
sphemakh <sphemakh@gmail.com>
2024-03-13 18:42:58 +0200

    add test init

--- tests/simple_cab.py
+++ tests/simple_cab.py
@@ -1,4 +1,11 @@
 from cultcargo import utils
+from scabha.basetypes import File
+import os.path
+testdir = os.path.dirname(os.path.abspath(__file__))
+
+oldcab = File(f"{testdir}/casa-mstransform.json")
+newcab = f"{oldcab.BASENAME}.yaml"
 schema = utils.SimpleCab()
-schema.init_from_old_cab("casa-mstransform.json")
-schema.save("casa-mstransform.yaml")
+schema.init_from_old_cab(oldcab)
+schema.save(newcab)
+
