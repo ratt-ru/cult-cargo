56fd136b64094a82d6fe594711a7ed6fb8034d49
landmanbester <lbester@sarao.ac.za>
2024-11-30 17:12:44 +0200

    sync pfb configs

--- cultcargo/genesis/pfb-imaging/sync_config.py
+++ cultcargo/genesis/pfb-imaging/sync_config.py
@@ -7,7 +7,7 @@ import cultcargo
 
 mpath = cultcargo.__file__.rstrip('__init__.py') + 'genesis/pfb-imaging/latest'
 
-branch = 'outputs'
+branch = 'v0.0.5-dev'
 content_path = f'https://raw.githubusercontent.com/ratt-ru/pfb-imaging/{branch}/pfb/parser'
 
 # returns list of files in subfolder
